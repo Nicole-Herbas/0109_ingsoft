fa8bca7e44c99860e7912be96102468433b1c7df
Nicole <nicole.herbas@ucb.edu.bo>
2026-09-01 10:15:30 -0400

    feat: si jugador1 tiene 1 punto y jugador2 tiene 0 puntos, imprime 15-Love

--- src/tenis.js
+++ src/tenis.js
@@ -1,7 +1,14 @@
 
 class TennisGame {
-  obtenerScore(){
-    return "Love-Love";
+  obtenerScore(ptj1, ptj2) {
+    let j1 = "Love";
+    let j2 = "Love";
+
+    if (ptj1 === 1) {
+      j1 = "15";
+    }
+
+    return j1 + "-" + j2;
   }
 }
 
--- src/tenis.test.js
+++ src/tenis.test.js
@@ -3,7 +3,12 @@ import TennisGame from './tenis';
 describe('Tennis Game', () => {
   it('debería imprimir "Love-Love" cuando ambos jugadores tienen 0 puntos', () => {
     let tennisGame = new TennisGame();
-    expect(tennisGame.obtenerScore()).toEqual('Love-Love');
+    expect(tennisGame.obtenerScore(0, 0)).toEqual('Love-Love');
+  });
+
+  it('debería imprimir "15-Love" cuando el jugador 1 tiene 1 punto y el jugador 2 tiene 0 puntos', () => {
+    let tennisGame = new TennisGame();
+    expect(tennisGame.obtenerScore(1, 0)).toEqual('15-Love');
   });
 });
 
